16ae78782799782e6cf6b0fad3b73980cd94385c
CherylL23 <Cheryl.Lee23@ycdsbk12.ca>
2023-01-23 13:01:26 -0500

    bufferedreader for csv

--- src/CSVfilereader.java
+++ src/CSVfilereader.java
@@ -1,9 +1,12 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class CSVfilereader {
     
-    public static void main(String args[]){
+    public static void main(String args[]) throws FileNotFoundException{
+        String path = "C:/Users/Chery/Downloads/annual-number-of-deaths-by-world-region - annual-number-of-deaths-by-world-region.csv";
         
+        BufferedReader reader = new BufferedReader(new FileReader(path));
     }
 }
