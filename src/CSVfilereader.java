825f6085f518f15ce28d440b21a36a893f296fa1
CherylL23 <Cheryl.Lee23@ycdsbk12.ca>
2023-01-23 13:08:30 -0500

    while loop to create array

--- src/CSVfilereader.java
+++ src/CSVfilereader.java
@@ -1,12 +1,19 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class CSVfilereader {
     
-    public static void main(String args[]) throws FileNotFoundException{
+    public static void main(String args[]) throws IOException{
+
         String path = "C:/Users/Chery/Downloads/annual-number-of-deaths-by-world-region - annual-number-of-deaths-by-world-region.csv";
-        
+        String line = "";
+
         BufferedReader reader = new BufferedReader(new FileReader(path));
+        
+        while((line = reader.readLine()) != null){
+            String [] values = line.split(",");
+            System.out.println(values[0]);
+        }
     }
 }
