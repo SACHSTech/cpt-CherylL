d4b07f5c479a7927829052127d50d04a8d76a152
CherylL23 <Cheryl.Lee23@ycdsbk12.ca>
2023-01-24 00:32:24 -0500

    while loop reads csv file

--- src/charts/CSVfilereader.java
+++ src/charts/CSVfilereader.java
@@ -1,26 +1,38 @@
 package charts;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CSVfilereader {
     
     public static void main(String args[]) throws IOException{
 
         String path = "C:/Users/Chery/Downloads/annual-number-of-deaths-by-world-region - annual-number-of-deaths-by-world-region.csv";
-        String line = "";
         String [][] data = csvArray(path, 3);
-        ArrayList<String> deathRecords = new ArrayList<String>();
         BufferedReader reader = new BufferedReader(new FileReader(path));
      
         for(int i = 0; i < data.length; i++){
             System.out.println(String.join(",", data[i]));
         }
-
-
     
     }
 
+    public static String [][] csvArray(String path, int columnNum) throws IOException{
+        
+        String currentLine;
+        String deLimiter = ",";
+        List <Integer> deathRecords = new ArrayList<Integer>();
+        FileReader fr = new FileReader(path);
+        BufferedReader reader = new BufferedReader(fr);
+
+        while((currentLine = reader.readLine()) != null){
+            
+        }
+        
+        return csvArray(path, columnNum);
+    }
   
 }
