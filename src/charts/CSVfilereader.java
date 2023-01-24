9a09f5f3fad0bcb806f47f9f96bfe297dc4ef697
CherylL23 <Cheryl.Lee23@ycdsbk12.ca>
2023-01-23 23:34:06 -0500

    changes

--- src/charts/CSVfilereader.java
+++ src/charts/CSVfilereader.java
@@ -1,6 +1,8 @@
+package charts;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CSVfilereader {
     
@@ -8,12 +10,17 @@ public class CSVfilereader {
 
         String path = "C:/Users/Chery/Downloads/annual-number-of-deaths-by-world-region - annual-number-of-deaths-by-world-region.csv";
         String line = "";
-
+        String [][] data = csvArray(path, 3);
+        ArrayList<String> deathRecords = new ArrayList<String>();
         BufferedReader reader = new BufferedReader(new FileReader(path));
-        
-        while((line = reader.readLine()) != null){
-            String [] values = line.split(",");
-            System.out.println(values[0]);
+     
+        for(int i = 0; i < data.length; i++){
+            System.out.println(String.join(",", data[i]));
         }
+
+
+    
     }
+
+  
 }
