a634a02200112d5ef8961ed0a29656f6338ba358
CherylL23 <Cheryl.Lee23@ycdsbk12.ca>
2023-02-03 02:10:27 -0500

    checkboxes added

--- src/charts/LineGraph.java
+++ src/charts/LineGraph.java
@@ -1,29 +1,26 @@
 package charts;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.Axis;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-/*public class LineGraph extends Application{
+public class LineGraph extends Application{
     
-    private LineChart chart;
+    LineChart<String,Number> lineChart;
     private CategoryAxis xAxis;
     private NumberAxis yAxis;
     XYChart.Series series2Line = new XYChart.Series();
@@ -34,136 +31,115 @@ import javafx.stage.Stage;
     XYChart.Series series4Line = new XYChart.Series();
 
     public Parent LineChart(){
-        String[] years = {"2017", "2018", "2019", "2020", "2021",};
-        FXCollections.<String>observableArrayList(years);
-
-        final LineChart<Number,Number> chart = 
-        chart.setTitle("Number of Deaths per Region in the past 5 years");
-        xAxis = new CategoryAxis();
-        yAxis = new NumberAxis();
+        
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
         xAxis.setLabel("Years");
-        yAxis.setLabel("number of deaths");
+        lineChart = new LineChart<String,Number>(xAxis,yAxis);
+        
+        lineChart.setTitle("Number of Deaths in the Past 5 Years in each Region");
+                          
+        series1Line.setName("Asia");
+        series1Line.getData().add(new XYChart.Data("2017", 21211975.55));
+        series1Line.getData().add(new XYChart.Data("2018", 21049466.45));
+        series1Line.getData().add(new XYChart.Data("2019", 20886957.35));
+        series1Line.getData().add(new XYChart.Data("2020", 20724448.25));
+        series1Line.getData().add(new XYChart.Data("2021", 20561939.16));
 
-
-
-        series1Line.setName("Oceana");
-        series1Line.getData().add(new XYChart.Data(years[0], 270970));
-        series1Line.getData().add(new XYChart.Data(years[1], 271264));
-        series1Line.getData().add(new XYChart.Data(years[2], 280924));
-        series1Line.getData().add(new XYChart.Data(years[3], 278401));
-        series1Line.getData().add(new XYChart.Data(years[4], 289468)); 
-
-   
-        series2Line.setName("Oceana");
-        series2Line.getData().add(new XYChart.Data(years[0], 270970));
-        series2Line.getData().add(new XYChart.Data(years[1], 271264));
-        series2Line.getData().add(new XYChart.Data(years[2], 280924));
-        series2Line.getData().add(new XYChart.Data(years[3], 278401));
-        series2Line.getData().add(new XYChart.Data(years[4], 289468));
-
-
+        series2Line.setName("Africa");
+        series2Line.getData().add(new XYChart.Data("2017", 10694839));
+        series2Line.getData().add(new XYChart.Data("2018", 10763424));
+        series2Line.getData().add(new XYChart.Data("2019", 10840634));
+        series2Line.getData().add(new XYChart.Data("2020", 11390416));
+        series2Line.getData().add(new XYChart.Data("2021", 12038022));
+      
+        
         series3Line.setName("Oceana");
-        series3Line.getData().add(new XYChart.Data(years[0], 270970));
-        series3Line.getData().add(new XYChart.Data(years[1], 271264));
-        series3Line.getData().add(new XYChart.Data(years[2], 280924));
-        series3Line.getData().add(new XYChart.Data(years[3], 278401));
-        series3Line.getData().add(new XYChart.Data(years[4], 289468));
+        series3Line.getData().add(new XYChart.Data("2017", 270970));
+        series3Line.getData().add(new XYChart.Data("2018", 271264));
+        series3Line.getData().add(new XYChart.Data("2019", 280924));
+        series3Line.getData().add(new XYChart.Data("2020", 278401));
+        series3Line.getData().add(new XYChart.Data("2021", 289468));
+        
 
+        series4Line.setName("Latin America and the Caribbean");
+        series4Line.getData().add(new XYChart.Data("2017", 4104061));
+        series4Line.getData().add(new XYChart.Data("2018", 4190846));
+        series4Line.getData().add(new XYChart.Data("2019", 4257246));
+        series4Line.getData().add(new XYChart.Data("2020", 5066101));
+        series4Line.getData().add(new XYChart.Data("2021", 5534706));
 
-        series4Line.setName("Latin America and the Caribbean (UN)");
-        series4Line.getData().add(new XYChart.Data(years[0], 270970));
-        series4Line.getData().add(new XYChart.Data(years[1], 271264));
-        series4Line.getData().add(new XYChart.Data(years[2], 280924));
-        series4Line.getData().add(new XYChart.Data(years[3], 278401));
-        series4Line.getData().add(new XYChart.Data(years[4], 289468));
+        series5Line.setName("Northern America");
+        series5Line.getData().add(new XYChart.Data("2017", 3049504));
+        series5Line.getData().add(new XYChart.Data("2018", 3064891));
+        series5Line.getData().add(new XYChart.Data("2019", 3063270));
+        series5Line.getData().add(new XYChart.Data("2020", 3544380));
+        series5Line.getData().add(new XYChart.Data("2021", 3579160));
 
+        series6Line.setName("Europe");
+        series6Line.getData().add(new XYChart.Data("2017", 8076159));
+        series6Line.getData().add(new XYChart.Data("2018", 8112356));
+        series6Line.getData().add(new XYChart.Data("2019", 8020246));
+        series6Line.getData().add(new XYChart.Data("2020", 9119281));
+        series6Line.getData().add(new XYChart.Data("2021", 9656398));
 
-        series5Line.setName("Latin America and the Caribbean (UN)");
-        series5Line.getData().add(new XYChart.Data(years[0], 270970));
-        series5Line.getData().add(new XYChart.Data(years[1], 271264));
-        series5Line.getData().add(new XYChart.Data(years[2], 280924));
-        series5Line.getData().add(new XYChart.Data(years[3], 278401));
-        series5Line.getData().add(new XYChart.Data(years[4], 289468));
-
-
-        series6Line.setName("Latin America and the Caribbean (UN)");
-        series6Line.getData().add(new XYChart.Data(years[0], 270970));
-        series6Line.getData().add(new XYChart.Data(years[1], 271264));
-        series6Line.getData().add(new XYChart.Data(years[2], 280924));
-        series6Line.getData().add(new XYChart.Data(years[3], 278401));
-        series6Line.getData().add(new XYChart.Data(years[4], 289468));
-
-        chart = new LineChart(xAxis,yAxis);
-        chart.getData().add(series6Line);
-        return chart;
-    }*/
+        lineChart.getData().addAll(series1Line, series2Line, series3Line, series4Line, series5Line, series6Line);
+        return lineChart;
+    }
 
 
     @Override public void start(Stage stage) throws Exception{
-
+        CheckBox box4 = new CheckBox("Asia");
+        CheckBox box5 = new CheckBox("Africa");
+        CheckBox box6 = new CheckBox("Oceana");
+        CheckBox box7 = new CheckBox("Latin America and the Caribbean");
+        CheckBox box8 = new CheckBox("Northern America");
+        CheckBox box9 = new CheckBox("Europe");
+        
+        Button helpButton = new Button("Refresh");
+        //helpButton.setOnAction(e -> handleOptions(box4,box5,box6, box7,box8,box9));
+        VBox layout1 = new VBox(20);
+        layout1.setPadding(new Insets(20,20,20,20));
+        layout1.getChildren().addAll(box4, box5, box6, box7,box8,box9, LineChart(), helpButton);
+        Scene scene1 = new Scene(layout1, 600, 600);
         stage.setTitle("Line Chart Sample");
-        final CategoryAxis xAxis = new CategoryAxis();
-        final NumberAxis yAxis = new NumberAxis();
-        xAxis.setLabel("Month");
-        final LineChart<String,Number> lineChart = 
-        new LineChart<String,Number>(xAxis,yAxis);
-       
-        lineChart.setTitle("Stock Monitoring, 2010");
-                          
-        XYChart.Series series1 = new XYChart.Series();
-        series1.setName("Portfolio 1");
-        
-        series1.getData().add(new XYChart.Data("Jan", 23));
-        series1.getData().add(new XYChart.Data("Feb", 14));
-        series1.getData().add(new XYChart.Data("Mar", 15));
-        series1.getData().add(new XYChart.Data("Apr", 24));
-        series1.getData().add(new XYChart.Data("May", 34));
-        series1.getData().add(new XYChart.Data("Jun", 36));
-        series1.getData().add(new XYChart.Data("Jul", 22));
-        series1.getData().add(new XYChart.Data("Aug", 45));
-        series1.getData().add(new XYChart.Data("Sep", 43));
-        series1.getData().add(new XYChart.Data("Oct", 17));
-        series1.getData().add(new XYChart.Data("Nov", 29));
-        series1.getData().add(new XYChart.Data("Dec", 25));
-        
-        XYChart.Series series2 = new XYChart.Series();
-        series2.setName("Portfolio 2");
-        series2.getData().add(new XYChart.Data("Jan", 33));
-        series2.getData().add(new XYChart.Data("Feb", 34));
-        series2.getData().add(new XYChart.Data("Mar", 25));
-        series2.getData().add(new XYChart.Data("Apr", 44));
-        series2.getData().add(new XYChart.Data("May", 39));
-        series2.getData().add(new XYChart.Data("Jun", 16));
-        series2.getData().add(new XYChart.Data("Jul", 55));
-        series2.getData().add(new XYChart.Data("Aug", 54));
-        series2.getData().add(new XYChart.Data("Sep", 48));
-        series2.getData().add(new XYChart.Data("Oct", 27));
-        series2.getData().add(new XYChart.Data("Nov", 37));
-        series2.getData().add(new XYChart.Data("Dec", 29));
-        
-        XYChart.Series series3 = new XYChart.Series();
-        series3.setName("Portfolio 3");
-        series3.getData().add(new XYChart.Data("Jan", 44));
-        series3.getData().add(new XYChart.Data("Feb", 35));
-        series3.getData().add(new XYChart.Data("Mar", 36));
-        series3.getData().add(new XYChart.Data("Apr", 33));
-        series3.getData().add(new XYChart.Data("May", 31));
-        series3.getData().add(new XYChart.Data("Jun", 26));
-        series3.getData().add(new XYChart.Data("Jul", 22));
-        series3.getData().add(new XYChart.Data("Aug", 25));
-        series3.getData().add(new XYChart.Data("Sep", 43));
-        series3.getData().add(new XYChart.Data("Oct", 44));
-        series3.getData().add(new XYChart.Data("Nov", 45));
-        series3.getData().add(new XYChart.Data("Dec", 44));
-        
-        Scene scene  = new Scene(lineChart,800,600);       
-        lineChart.getData().addAll(series1, series2, series3);
+        //Scene scene  = new Scene(scene1);       
         
        
-        stage.setScene(scene);
+        stage.setScene(scene1);
         stage.show();
     }
- 
+
+    /*private Object handleOptions(CheckBox box4, CheckBox box5, CheckBox box6, CheckBox box7, CheckBox box8, CheckBox box9) {
+        if(box4.isSelected() == true){ 
+            lineChart.getData().add(series1);
+            
+        
+          } else if(box1.isSelected() == false){
+            lineChart.getData().remove(series1);
+      
+          }
+      
+          if(box2.isSelected()) {
+              barChart.getData().add(series2);
+      
+          }else if(box2.isSelected() == false){
+            barChart.getData().remove(series2);
+       
+          }
+      
+          if(box3.isSelected()) {
+            barChart.getData().add(series3);
+          }
+          else if (box3.isSelected() == false) {
+            barChart.getData().remove(series3);
+          
+          }*/
+      
+    
+    //}
+
+
     public static void main(String[] args) {
         launch(args);
     }
